2a23cacc5e33ccc8ffdcb29c6973d8b358c60162
Vinayak K <vinayakkurugovinakoppa@qnap.com>
2022-04-06 09:06:33 +0530

    Synchronous consumers

---
+++ my_channel/my_channel/asgi.py
@@ -0,0 +1,36 @@
+"""
+ASGI config for my_channel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_channel.settings')
+
+# application = get_asgi_application()
+
+# mysite/asgi.py
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+import chat.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_channel.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
